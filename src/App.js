5a69c7820d38f0814d8fc773634d71a8928861ac
GarrettGrim111 <garrett.grim111@gmail.com>
2020-08-11 20:58:22 +0200

    created separated pages for shop, collection-preview component and shop data database, designed basic routing, yet not fully operational, databes mapped dynamically

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch, Redirect } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
-      {" "}
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
+
+      {/* <Route exact path="/jackets" component={Jackets} />
+        <Route exact path="/sneakers" component={Sneakers} />
+        <Route exact path="/mens" component={Mens} />
+        <Route exact path="/womens" component={Womens} />
+        <Route exact path="/not-found" component={NotFound} /> */}
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./menu-item.styles.scss";
+import { withRouter } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
-    <div className={`${size} menu-item`}>
+    <div
+      className={`${size} menu-item`}
+      onclick={() => history.push(`${match.url}${linkUrl}`)}
+    >
       <div
         className="background-image"
         style={{
@@ -12,11 +16,11 @@ const MenuItem = ({ title, imageUrl, size }) => {
       />
 
       <div className="content">
-        <h1 className="title">{title}</h1>
+        <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
       </div>
     </div>
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
